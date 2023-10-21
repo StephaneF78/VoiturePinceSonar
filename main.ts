8fb84f590f60afe658c15aa71b9eb99c18f7ec12
Stéphane Biaggini <stephane.biaggini@free.fr>
2023-10-21 18:52:09 +0200

    avec servo et ultrason

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+let VarPosServo1pince = 0
 basic.forever(function () {
-	
+    magicbit.Servo(magicbit.Servos.S1, VarPosServo1pince)
+    basic.showNumber(magicbit.Ultrasonic(DigitalPin.P0))
+    basic.pause(200)
 })
